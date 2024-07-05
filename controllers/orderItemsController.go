b0d9a4733cf0a173f21bb70e871c5c00f53b0990
dcoder13 <divyanshbisht2005@gmail.com>
2024-07-05 17:15:55 +0530

    models

--- controllers/orderItemsController.go
+++ controllers/orderItemsController.go
@@ -1,2 +1,37 @@
 package controller
 
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func GetOrderItems() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetOrderItemsByOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+func GetOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+func CreateOrderItems() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+func updateOrderItems() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func ItemsByOrder(id string) (OrderItems []primitive.M, err error) {
+
+}
